da8b415160f8219412d478042af74169cfbea8b2
angelaoica-dev <oicacm06@hotmail.com~>
2025-08-09 21:07:44 -0600

    Agregué funcion 'agregarAmigo'

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+// Array global para almacenar los amigos.
 let amigos = [];
+
+function agregarAmigo(){
+    //Obtener el valor del input.
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+
+    //Validar que el campo no esté vacío.
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.")
+       return; 
+    }
+        
+    //Agregar el nombre al array de amigos
+    amigos.push(nombre);
+    
+    //Mostrar la lista actualizada
+    mostrarAmigos();
+
+    input.value = "";
+    }
